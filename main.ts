accb9b4d4f5317c42fe5c95c192669c50d9c46da
andrey776suarez <99951372+andrey776suarez@users.noreply.github.com>
2022-04-04 13:25:55 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ input.onButtonPressed(Button.A, function () {
     music.playTone(294, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half))
 })
 basic.forever(function () {
 	
